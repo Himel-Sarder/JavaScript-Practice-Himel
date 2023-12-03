0aa275f2530b89f72d35552dd1be2e1183fdb571
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2023-12-03 23:44:13 +0600

    JavaScript Himel

---
+++ JavaScript/Data Type.js
@@ -0,0 +1,19 @@
+/*
+There are three types of Data type-
+1. String
+2. Number
+3. Boolean
+*/
+
+var name = "Himel";
+var string1 = "Sar";
+var string2 = "der";
+var id = 22111121;
+var num1 = 5;
+var num2 = 10;
+var bool = true; //Not True
+console.log(name);
+console.log(id);
+console.log(num1+num2);
+console.log(bool);
+console.log(string1+string2);
